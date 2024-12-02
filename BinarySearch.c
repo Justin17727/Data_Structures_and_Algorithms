28bd0458d61f9fb1de7459a54ea8698ec6bab92d
Justin Joseph <lalalolowhatever@gmail.com>
2024-12-02 23:34:43 +0530

    added one more program

---
+++ BinarySearch.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+int binarySearch(int* arr, int key, int size);
+int compare(const void* a, const void* b);
+int main(){
+    int n, b;
+    printf("Enter the number of elements of array: ");
+    scanf("%d", &n);
+    int a[n];
+    for(int i=0; i<n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &a[i]);
+    }
+    qsort(a, n, sizeof(int), compare);
+    printf("Enter element to be searched: ");
+    scanf("%d", &b);
+    int index = binarySearch(a, b, n);
+    index != -1 ? printf("Element found at index %d", index) : printf("Element not found");
+}
+int binarySearch(int* arr, int key, int size){
+    int low = 0, high = size-1, mid;
+    while(low <= high){
+        mid = (low+high)/2;
+        printf("low: %d\thigh: %d\tmid: %d\n", low, high, mid);
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] < key){
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return -1;
+}
+int compare(const void* a, const void* b){
+    int* x = (int*)a;
+    int* y = (int*)b;
+    return *x-*y;
+}
