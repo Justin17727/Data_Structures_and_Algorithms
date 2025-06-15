92de3bedba7bff4f8bf5d6d9cda6296c0108623b
Justin Joseph <lalalolowhatever@gmail.com>
2025-06-15 20:59:17 +0530

    new program

---
+++ Simple_Linear_Queue-using-array.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#define MAX 100
+int arr[100];
+void enqueue(int* front, int* rear, int item);
+int dequeue(int* top, int* rear, int* status);
+void peek(int front, int rear);
+void display(int top, int rear);
+int main(){
+    int ch, data, dequeued_element = 0, front = -1, rear = -1;
+    while(1){
+        printf("Enter 1 to enqueue\n");
+        printf("Enter 2 to dequeue\n");
+        printf("Enter 3 to peek\n");
+        printf("Enter 4 to display\n");
+        printf("Enter 5 to exit\n");
+        printf("\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch){
+            case 1:
+                printf("Enter data to be enqueued: ");
+                scanf("%d", &data);
+                enqueue(&front, &rear, data);
+                break;
+            case 2:
+                data = dequeue(&front, &rear, &dequeued_element);
+                if(dequeued_element){
+                    printf("Removed %d from queue\n", data);
+                }
+                break;
+            case 3:
+                peek(front, rear);
+                break;
+            case 4:
+                display(front, rear);
+                break;
+            case 5:
+                return 0;
+            default:
+                printf("Invalid choice.");
+        }
+        printf("\n");
+    }
+}
+void enqueue(int* front, int* rear, int item){
+    if(*rear == MAX - 1){
+        printf("Overflow!\n");
+        return;
+    }
+    if(*front == -1){
+        *front = 0;
+    }
+    arr[++(*rear)] = item;
+}
+int dequeue(int* front, int* rear, int* status){
+    int item = 0;
+    if(*front == -1 || *front > *rear){
+        printf("Underflow!\n");
+        *status = 0;
+        return item;
+    }
+    item = arr[(*front)++];
+    *status = 1;
+    return item;
+}
+void peek(int front, int rear){
+    if(front == -1 || front > rear){
+        printf("--Empty--\n");
+        return;
+    }
+    printf("Peeked element: %d\n", arr[front]);
+}
+void display(int front, int rear){
+    if(front == -1 || front > rear){
+        printf("--Empty--\n");
+        return;
+    }
+    printf("Queue: ");
+    for(int i = front; i < rear; i++){
+        printf("%d, ", arr[i]);
+    }
+    printf("%d\n", arr[rear]);
+}
