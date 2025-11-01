fde74181727a947ef99fc50584ffea71c40d19e9
Justin Joseph <lalalolowhatever@gmail.com>
2025-11-01 22:55:07 +0530

    new program

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,46 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int binarySearch(int* arr, int key, int size);
 int compare(const void* a, const void* b);
+
 int main(){
-    int n, b;
+    
+    int n;  // number of elements of the array
+    int b;  // element to be searched
+    
     printf("Enter the number of elements of array: ");
     scanf("%d", &n);
+    
+    // initialising an array of n elements
     int a[n];
     for(int i = 0; i < n; i++){
         printf("Enter element %d: ", i + 1);
         scanf("%d", &a[i]);
     }
+    
+    // sorting the array
     qsort(a, n, sizeof(int), compare);
+    
     printf("Enter element to be searched: ");
     scanf("%d", &b);
+    
+    // index of the element to be searched
     int index = binarySearch(a, b, n);
     index != -1 ? printf("Element found at index %d", index) : printf("Element not found");
 }
+
+// search a key in given array of given size, return key's index
 int binarySearch(int* arr, int key, int size){
-    int low = 0, high = size - 1, mid;
+
+    int low = 0;  // lower bound (index) to be considered
+    int high = size - 1;  // upper bound (index) to be considered
+    int mid;  // element's index in question being key or not
+
+    // lower bound should always be lesser or equal to upper bound
     while(low <= high){
+
+        // middle element of given bounds
         mid = (low + high) / 2;
+
+        // if the middle element is the key
         if(arr[mid] == key){
             return mid;
         }
+
+        // if the middle element is smaller than key, then key might exist after it
         else if(arr[mid] < key){
             low = mid + 1;
         }
+
+        // if the middle element is larger than key, then key might exist before it
         else{
             high = mid - 1;
         }
     }
+
+    // the key does'nt exist in the given array
     return -1;
 }
+
+// function to compare two elements given the pointers to them for qsort()
 int compare(const void* a, const void* b){
+    
+    // convert the two void pointers to integer pointers
     int* x = (int*)a;
     int* y = (int*)b;
+    
     if(*x < *y){
-        return -1;
+        return -1;  // signifying y should come before x
     }
+
     if(*x > *y){
-        return 1;
+        return 1;  // signifying x should come before y
     }
-    return 0;
+
+    return 0;  // signifying x and y are equal for sorting
 }
---
+++ ShuntingYard.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+char stack[100];  // array stack
+int top = -1;     // top pointer of stack
+
+// push symbol to stack
+void push(char x){
+
+    // overflow check
+    if(top == 99){
+        printf("Stack overflow!");
+        printf("Exiting the program.\n");
+        exit(2);
+    }
+
+    stack[++top] = x;
+}
+
+// pop symbol from stack
+char pop(){
+    if(top == -1){
+        return '\0';
+    }
+    else{
+        return stack[top--];
+    }
+}
+
+// symbol priority, lower value for low priority
+int priority(char x){
+    switch(x){
+        case '(':
+            return 0;
+        case '+':
+            return 1;
+        case '-':
+            return 1;
+        case '*':
+            return 2;
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
+    }
+}
+
+int main(){
+    
+    char exp[100];  // buffer for input expression
+    char* e;        // pointer to traverse string buffer 'exp'
+    char x;         // popped symbol from stack
+    
+    // input from user
+    printf("Enter the expression : ");
+    fgets(exp, 100, stdin);
+
+    // remove '\n' from string buffer
+    for(int i = 0; i < 100; i++){
+        if(exp[i] == '\n'){
+            exp[i] = '\0';
+            break;
+        }
+
+        // if input too long, '\n' not taken in buffer
+        if(i == 99){
+            printf("Entered expression too long!\n");
+            printf("Exiting the program.\n");
+            exit(2);
+        }
+    }
+    
+    e = exp;  // initialise pointer 'e' from start address of 'exp'
+
+    // while not reached the end of string buffer
+    while(*e != '\0'){
+        // ignore whitespace character
+        if(*e == ' '){
+            *e++;  // increment pointer to point to nect character in string buffer
+            continue;
+        }
+
+        // display alpha-numeric character
+        else if(isalnum(*e)){
+            printf("%c",*e);
+        }
+
+        // if open parentheses then push symbol to stack
+        else if(*e == '('){
+            push(*e);
+        }
+
+        // if close parentheses then pop & display symbols until encounter open parentheses
+        else if(*e == ')'){
+            while((x = pop()) != '(')
+                printf("%c", x);
+        }
+
+        // if encounter a symbol
+        else{
+
+            // if symbol priority lower than or equal to top symbol in stack
+            while(priority(stack[top]) >= priority(*e)){
+
+                // if symbol and top element of stack are '^'
+                if(priority(*e) == 3 && priority(stack[top]) == 3){
+                    break;
+                }
+
+                // if both not '^'
+                else{
+                    printf("%c", pop());
+                }
+            }
+
+            // push symbol to stack
+            push(*e);
+        }
+        
+        e++;  // increment pointer to point at next character in string buffer
+    }
+
+    // if stack not empty
+    while(top != -1){
+        printf("%c", pop());
+    }
+    printf("\n");
+
+    return 0;
+}
